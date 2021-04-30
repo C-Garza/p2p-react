4691f3060e94b1862bc82bb285ef292cc90a8c0d
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-04-30 03:45:35 -0700

    Setup request for new uuid room

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
+const {v4: uuidV4} = require("uuid");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.get("/api/uuid", (req, res) => {
+  res.send({id: uuidV4()});
+});
+
+io.on("connection", socket => {
+  socket.on("join-room", (roomID, userID) => {
+    console.log(roomID, userID);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server is listening on ${port}`);
