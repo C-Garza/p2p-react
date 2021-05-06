b3f24d02d73cc3d4ba11fa0117a21c0f2499696f
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-05-06 02:54:31 -0700

    Add listeners for showing host made room name and add listener for stream-ready to fix race condition bug

--- server/sockets/peerSocket.js
+++ server/sockets/peerSocket.js
@@ -1,5 +1,6 @@
 const socketio = require("socket.io");
 const {addUser, removeUser, getUsersInRoom} = require("../utils/users");
+const {addRoom, getRoom, removeRoom} = require("../utils/rooms");
 
 const peerSocketConnection = (server) => {
   const io = socketio(server, {
@@ -10,25 +11,40 @@ const peerSocketConnection = (server) => {
   });
   
   io.on("connection", socket => {
-    socket.on("join-room", (roomID, userID, displayName) => {
-      console.log(roomID, userID, displayName);
+    socket.on("join-room", (roomID, userID, displayName, roomName) => {
+      console.log(roomID, userID, displayName, roomName);
       socket.join(roomID);
       
       addUser(userID, socket.id, displayName, roomID);
+      if(roomName.length) addRoom(roomID, roomName);
+
       const users = getUsersInRoom(roomID);
+      const room = getRoom(roomID);
       
+      // SEND USERS IN ROOM TO CLIENT
       io.to(roomID).emit("users-list", users);
 
-      socket.broadcast.to(roomID).emit("user-connected", userID);
+      // BROADCAST TO ROOM A NEW CLIENT HAS JOINED
+      socket.on("stream-ready", () => {
+        socket.broadcast.to(roomID).emit("user-connected", userID);
+      });
+
+      // SEND HOST ROOM NAME IF NOT HOST
+      if(!roomName.length) {
+        io.to(socket.id).emit("room-name", room);
+      }
   
       socket.on("disconnect", () => {
         socket.broadcast.to(roomID).emit("user-disconnected", userID);
+
+        removeUser(socket.id);
+
+        // IF NO ONE IS IN ROOM, REMOVE ROOM
+        if(!io.sockets.adapter.rooms.get(roomID)?.size) {
+          removeRoom(roomID);
+        }
       });
     });
-    
-    socket.on("disconnect", () => {
-      removeUser(socket.id);
-    })
   });
 };
 
---
+++ server/utils/rooms.js
@@ -0,0 +1,21 @@
+let rooms = {};
+
+const addRoom = (id, name) => {
+  if(!rooms[id]) rooms[id] = name;
+};
+
+const removeRoom = (id) => {
+  let {[id]: tmp, ...rest} = rooms;
+
+  rooms = rest;
+};
+
+const getRoom = (id) => {
+  return rooms[id];
+};
+
+module.exports = {
+  addRoom,
+  removeRoom,
+  getRoom
+};
