30168c1fa79769594f0cf37e56aa1fb82d1d9588
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-05-13 22:43:41 -0700

    Add path file to build folder

--- server/servers/app.js
+++ server/servers/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const http = require("http");
 const {v4: uuidV4} = require("uuid");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "streams-react/build")));
 
 app.use(cors());
 
@@ -11,6 +14,10 @@ const server = http.createServer(app);
 
 app.get("/api/uuid", (req, res) => {
   res.send({id: uuidV4()});
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname+"/streams-react/build/index.html"));
 });
 
 module.exports = server;
