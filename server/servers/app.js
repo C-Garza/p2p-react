b87c81d6f4a167e50987214a326de957a10db58d
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-05-13 23:23:41 -0700

    Fix directory paths

--- server/servers/app.js
+++ server/servers/app.js
@@ -6,7 +6,7 @@ const {v4: uuidV4} = require("uuid");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "streams-react/build")));
+app.use(express.static(path.join(__dirname, "..", "..", "streams-react/build")));
 
 app.use(cors());
 
@@ -17,7 +17,7 @@ app.get("/api/uuid", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname+"/streams-react/build/index.html"));
+  res.sendFile(path.join(__dirname+"..","..","/streams-react/build/index.html"));
 });
 
 module.exports = server;
